3c15530f11c1d620218d803429b729c57a621840
hwang <2567667207@qq.com>
2020-04-29 15:32:49 +0800

    新增，Hystrix,zuul

--- cloud-demo/consumer-demo/src/main/java/cn/itcast/ConsumerApplication.java
+++ cloud-demo/consumer-demo/src/main/java/cn/itcast/ConsumerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,7 @@ import java.util.concurrent.RunnableFuture;
 //@SpringBootApplication
 //@EnableDiscoveryClient
 
+@EnableFeignClients
 @SpringCloudApplication//上面三个都有了
 public class ConsumerApplication {
 
@@ -25,13 +27,13 @@ public class ConsumerApplication {
      * RestTemplate可以调用三种Http
      * @return
      */
-    @Bean
-    @LoadBalanced//负载均衡，会内置一个拦截器拦截RestTemplate的Http请求
-    public RestTemplate restTemplate() {
-        // 这次我们使用了OkHttp客户端,只需要注入工厂即可
-        //return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
-        return new RestTemplate();
-    }
+//    @Bean
+//    @LoadBalanced//负载均衡，会内置一个拦截器拦截RestTemplate的Http请求
+//    public RestTemplate restTemplate() {
+//        // 这次我们使用了OkHttp客户端,只需要注入工厂即可
+//        //return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
+//        return new RestTemplate();
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class,args);
--- cloud-demo/consumer-demo/src/main/java/cn/itcast/consumer/web/ConsumerController.java
+++ cloud-demo/consumer-demo/src/main/java/cn/itcast/consumer/web/ConsumerController.java
@@ -1,9 +1,10 @@
 package cn.itcast.consumer.web;
 
+import cn.itcast.consumer.client.UserClient;
 import cn.itcast.consumer.pojo.User;
-import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+//import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+//import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
@@ -17,34 +18,50 @@ import java.util.List;
 @RestController
 @RequestMapping("consumer")
 @Slf4j
-@DefaultProperties(defaultFallback = "defaultFallback")
+//@DefaultProperties(defaultFallback = "defaultFallback")
 public class ConsumerController {
 
-    @Autowired
-    private RestTemplate restTemplate;//调用三种Http
+//    @Autowired
+//    private RestTemplate restTemplate;//调用三种Http
 
 //    @Autowired
 //    private RibbonLoadBalancerClient client;
 
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private UserClient userClient;
+
     @GetMapping("{id}")
+    public User queryById(@PathVariable("id") Long id) {
+        return userClient.queryById(id);
+
+    }
+
+
+//    @GetMapping("{id}")
     //@HystrixCommand(fallbackMethod = "queryByIdFallback")//失败之后的指令，开启服务的线程隔离和降级处理
     /*@HystrixCommand(commandProperties = {
             @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
     })*/
-    @HystrixCommand(commandProperties = {
-                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),//错误次数
-                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),//错误时长
-                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60")//错误百分比
-            })
-    public String queryById(@PathVariable("id") Long id) {
-        if(id%2==0){
-            throw new  RuntimeException("");
-        }
-        String url="http://user-service/user/"+id;
-        //User user = restTemplate.getForObject(url, User.class);
-        String user = restTemplate.getForObject(url, String.class);
-        return user;
-    }
+//    @HystrixCommand(commandProperties = {
+//                    //熔断器请求次数阈值，触发熔断的最小请求次数
+//                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),
+//                    //休眠时长，熔断器打开后会等一段休眠时间之后进入half open状态，放一部分请求过去
+//                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),
+//                    //错误百分比
+//                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60")
+//            })
+//    public String queryById(@PathVariable("id") Long id) {
+//        if(id%2==0){
+//            throw new  RuntimeException("");
+//        }
+//        String url="http://user-service/user/"+id;
+//        //User user = restTemplate.getForObject(url, User.class);
+//        String user = restTemplate.getForObject(url, String.class);
+//        return user;
+//    }
 
     public String queryByIdFallback(Long id) {
         log.error("查询用户信息失败，id:{}",id);
