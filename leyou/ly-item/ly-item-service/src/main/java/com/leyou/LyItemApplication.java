b51968432585f05cbfd482c48575523c387c808a
hwang <2567667207@qq.com>
2020-05-25 17:27:28 +0800

    pom里写错依赖了，spring-cloud-starter-netflix-eureka-client和spring-cloud-netflix-eureka-client是两种依赖，用idea的自动提示很容易不注意就用错

---
+++ leyou/ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
@@ -0,0 +1,9 @@
+package com.leyou;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LyItemApplication {
+}
