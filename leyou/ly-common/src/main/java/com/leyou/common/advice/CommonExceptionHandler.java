b0003506a9b5afb13096363186a7a7c19b536f70
hwang <2567667207@qq.com>
2020-05-29 17:51:51 +0800

    自定义异常

--- leyou/ly-common/src/main/java/com/leyou/common/advice/CommonExceptionHandler.java
+++ leyou/ly-common/src/main/java/com/leyou/common/advice/CommonExceptionHandler.java
@@ -2,6 +2,7 @@ package com.leyou.common.advice;
 
 import com.leyou.common.enums.ExceptionEnum;
 import com.leyou.common.exception.LyException;
+import com.leyou.common.vo.ExceptionResult;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,11 +11,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice//默认自动拦截所有controller的
 public class CommonExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleException(LyException e){
+    @ExceptionHandler(LyException.class)
+    public ResponseEntity<ExceptionResult> handleException(LyException e){
         ExceptionEnum em = e.getExceptionEnum();
         //return ResponseEntity.status(400).body();
-        return ResponseEntity.status(em.getCode()).body(em.getMsg());
+        return ResponseEntity.status(em.getCode()).body(new ExceptionResult(e.getExceptionEnum()));
 
     }
 }
